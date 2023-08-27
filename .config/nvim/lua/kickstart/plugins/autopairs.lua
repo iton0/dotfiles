036e56c6e1242f795991be13459b12824b28bba9
iton0 <iton442@gmail.com>
2023-08-27 19:45:04 -0400

    Prevent Backspace Conflict:
    Disable 'autopairs' Backspace mapping to ensure smooth functionality with 'vim-visual-multi' plugin.

--- .config/nvim/lua/kickstart/plugins/autopairs.lua
+++ .config/nvim/lua/kickstart/plugins/autopairs.lua
@@ -1,10 +1,11 @@
-
 return {
   "windwp/nvim-autopairs",
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
     require("nvim-autopairs").setup {}
+    -- Disable autopairs mapping for Backspace
+    vim.g.AutoPairsShortcutBackInsert = 0
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
