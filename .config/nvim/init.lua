059a0ff29fc95dffd306f3d7a936b46612cbc956
iton0 <iton442@gmail.com>
2023-08-29 16:51:12 -0400

    Enabled cursorline highlighting
    Current line is highlighted a different color
    to better visual and see which line cursor is on.
    For better readability.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -244,6 +244,9 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
+-- Enable cursorline highlighting
+vim.wo.cursorline = true
+
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
