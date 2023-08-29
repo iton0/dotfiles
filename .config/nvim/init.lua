b5585496c77f03b56aa06ec43e4f27197be636b2
iton0 <iton442@gmail.com>
2023-08-29 10:31:19 -0400

    Updated Colorscheme:
    Using the storm varient of TokyoNight color scheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,7 +129,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
