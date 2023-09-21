c372f0d3a8a25f075b0397423858146142a72490
iton0 <iton442@gmail.com>
2023-09-21 17:52:35 -0400

    initial commit of wezterm
    commit of wezterm config file
    uses lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,10 +12,64 @@ end
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night Storm (Gogh)'
+-- Color and Appearance
+config.color_scheme = 'Tokyo Night Day (Gogh)'
 
-config.window_background_opacity = 0.5
+config.anti_alias_custom_block_glyphs = true
+config.bold_brightens_ansi_colors = "BrightAndBold"
+
+-- Window Setting
+config.initial_cols = 80
+config.initial_rows = 24
+
+
+config.window_background_image = '/home/iton/.config/wezterm/background.gif'
+
+config.window_background_image_hsb = {
+	-- Darken the background image by reducing it to 1/3rd
+	brightness = 0.3,
+
+	-- You can adjust the hue by scaling its value.
+	-- a multiplier of 1.0 leaves the value unchanged.
+	hue = 1.0,
+
+	-- You can adjust the saturation also.
+	saturation = 1.0,
+}
+
+config.window_padding = {
+	left = '0cell',
+	right = '0cell',
+	top = '0cell',
+	bottom = '0cell',
+}
+
+config.window_frame = {
+	inactive_titlebar_bg = '#1a1b26',
+	active_titlebar_bg = '#1f2335',
+	inactive_titlebar_fg = '#6b7280',
+	active_titlebar_fg = '#ffffff',
+	inactive_titlebar_border_bottom = '#1f2335',
+	active_titlebar_border_bottom = '#1f2335',
+	button_fg = '#6b7280',
+	button_bg = '#1f2335',
+	button_hover_fg = '#ffffff',
+	button_hover_bg = '#292d47',
+}
+
+config.enable_tab_bar = false
+config.hide_mouse_cursor_when_typing = true
+
+config.enable_wayland = false
+
+
+-- Font
+config.font =
+    wezterm.font('FiraCode Nerd Font')
+
+config.font_size = 15
+config.cell_width = 1.0
+config.line_height = 1.0
 
 -- and finally, return the configuration to wezterm
 return config
