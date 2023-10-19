cecc65c139f8c5250102d618780876905907b3d0
iton0 <iton442@gmail.com>
2023-10-19 12:28:36 -0400

    feat(nvim): Consolidate indentation settings
    This commit consolidates the indentation settings in the `init.lua` file. It adds autocmd events for various file types to adjust the tabstop, shiftwidth, and expandtab settings. This will help maintain consistent indentation across different programming languages.
    
    Changes:
    - Consolidated indentation settings for JavaScript, C++, Java, Python, HTML, CSS, and Lua.
    - Added autocmd events for adjusting indentation settings based on file type.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -44,6 +19,20 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- Add your autocmd events here for adjusting indentation settings
+local function set_indentation(filetype, tabstop, shiftwidth)
+  vim.api.nvim_command('autocmd FileType ' ..
+    filetype .. ' setlocal tabstop=' .. tabstop .. ' shiftwidth=' .. shiftwidth .. ' expandtab')
+end
+
+set_indentation('javascript', 2, 2)
+set_indentation('cpp', 4, 4)
+set_indentation('java', 4, 4)
+set_indentation('python', 4, 4)
+set_indentation('html', 2, 2)
+set_indentation('css', 2, 2)
+set_indentation('lua', 2, 2)
 
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
