fffab3e732f7359ede5b25fa29d38b36727bf37d
iton0 <iton442@gmail.com>
2023-10-22 13:18:49 -0400

    refactor: Update Lualine separators
    In this commit, the Lualine component and section separators have been
    refactored in the init.lua configuration file. The component separators
    have been changed to '||' for both the left and right sides, resulting
    in a more streamlined appearance. This change enhances the visual
    consistency and aesthetics of the Neovim statusline.
    
    - Updated Lualine component separators
    - Improved the appearance of the statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,7 +168,7 @@ require('lazy').setup({
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '\\ ', right = ' //' },
+        component_separators = { left = '||', right = '||' },
         section_separators = '',
       },
     },
