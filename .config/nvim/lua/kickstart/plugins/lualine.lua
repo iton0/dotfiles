84fce7bf0978b5dea37942eca521eaa524d7fa16
iton0 <iton442@gmail.com>
2023-10-22 22:43:41 -0400

    Simplify lualine separators and add new sections
    In this commit, we made the following changes to 'lualine.lua':
    
    - Removed the custom left and right component separators.
    - Added new sections 'lualine_a' and 'lualine_c'.
      - 'lualine_a' includes a customized 'mode' display.
      - 'lualine_c' displays the 'filetype' with padding, color, and icon-only mode, followed by the 'filename'.
    - Reorganized the 'lualine_x' section to display 'filetype', 'fileformat', and 'encoding'.
    
    These changes simplify the configuration and enhance the visual representation of the lualine status line in Neovim.

--- .config/nvim/lua/kickstart/plugins/lualine.lua
+++ .config/nvim/lua/kickstart/plugins/lualine.lua
@@ -9,12 +9,28 @@ return {
     options = {
       icons_enabled = true,
       theme = 'auto',
-      component_separators = { left = '||', right = '||' },
+      component_separators = '',
       section_separators = '',
     },
     sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = function(str) return str:sub(1, 1) end,
+        }
+      },
+      lualine_c = {
+        {
+          "filetype",
+          padding = { left = 1, right = 0 },
+          colored = true,
+          icon_only = true,
+        },
+        {
+          'filename',
+        },
+      },
       lualine_x = {
-        'filetype',
         'fileformat',
         'encoding',
       }
