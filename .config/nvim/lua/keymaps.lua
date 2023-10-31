41ee2e0dddc9f44ffbe8fadd180b004b64d3578b
iton0 <iton442@gmail.com>
2023-10-31 09:31:15 -0400

    feat(keymap): Add key mapping for split navigation
    In the 'keymaps.lua' file, added custom key mappings to facilitate split navigation. These mappings use the leader key followed by directional keys to move between splits:
    
    - `<leader>th` for moving to the split on the left.
    - `<leader>tj` for moving to the split below.
    - `<leader>tk` for moving to the split above.
    - `<leader>tl` for moving to the split on the right.
    
    Each key mapping is non-recursive and includes a description for clarity.
    
    This enhances the usability of Neovim for navigating between split windows.
    
    See the specific key mappings in the 'keymaps.lua' file for more details.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -9,6 +9,12 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Remap for moving between splits
+vim.keymap.set('n', '<leader>th', '<C-w>h', { noremap = true, silent = true, desc = 'Move to split on left' })
+vim.keymap.set('n', '<leader>tj', '<C-w>j', { noremap = true, silent = true, desc = 'Move to split below' })
+vim.keymap.set('n', '<leader>tk', '<C-w>k', { noremap = true, silent = true, desc = 'Move to split above' })
+vim.keymap.set('n', '<leader>tl', '<C-w>l', { noremap = true, silent = true, desc = 'Move to split on right' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
