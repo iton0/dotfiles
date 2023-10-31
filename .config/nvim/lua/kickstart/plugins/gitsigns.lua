732135f4a80b6525503262b7165d99836944f38b
iton0 <iton442@gmail.com>
2023-10-31 13:09:52 -0400

    refactor: Update gitsigns.lua configuration
    Made changes to the gitsigns.lua configuration in the Neovim setup.
    Made untracked sign more visible

--- .config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ .config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -5,11 +5,12 @@ return {
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      add = { text = ' +' },
-      change = { text = ' |' },
-      delete = { text = ' _' },
-      topdelete = { text = ' ‾' },
+      add          = { text = ' +' },
+      change       = { text = ' |' },
+      delete       = { text = ' _' },
+      topdelete    = { text = ' ‾' },
       changedelete = { text = ' ~' },
+      untracked    = { text = ' ┆' },
     },
     on_attach = function(bufnr)
       vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
