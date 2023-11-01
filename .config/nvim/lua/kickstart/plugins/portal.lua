0a1bb4ed6ec99f3c6a4c4479993cffee71d6d51b
iton0 <iton442@gmail.com>
2023-11-01 19:52:42 -0400

    refactor: Neovim Configuration Plugins
    - Optimized the configuration for Neovim plugins.
    - Improved code structure and formatting for better readability.
    - Made adjustments to enhance plugin functionality and compatibility.
    - Simplified event handling and removed unnecessary comments.
    - Organized the code in autoformat.lua, portal.lua, and theme.lua files for improved maintainability and understanding.
    
    This commit streamlines the configuration of Neovim plugins to enhance the overall user experience.

--- .config/nvim/lua/kickstart/plugins/portal.lua
+++ .config/nvim/lua/kickstart/plugins/portal.lua
@@ -2,15 +2,15 @@ return {
 	-- Improved location list navigation
 	-- Labelled floating window showing a snippet of some buffer
 	'cbochs/portal.nvim',
-	event        = { 'BufReadPre', 'BufNewFile' },
+	event = { 'BufReadPre', 'BufNewFile' },
 	-- Optional dependencies
 	dependencies = {
 		'cbochs/grapple.nvim',
-		'ThePrimeagen/harpoon'
+		'ThePrimeagen/harpoon',
 	},
-	config       = function()
+	config = function()
 		-- Enable opening portals both forwards and backwards for portal.nvim
-		vim.keymap.set("n", "<leader>o", "<cmd>Portal jumplist backward<cr>")
-		vim.keymap.set("n", "<leader>i", "<cmd>Portal jumplist forward<cr>")
-	end
+		vim.keymap.set('n', '<leader>o', '<cmd>Portal jumplist backward<cr>')
+		vim.keymap.set('n', '<leader>i', '<cmd>Portal jumplist forward<cr>')
+	end,
 }
