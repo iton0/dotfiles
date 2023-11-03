68a6703e38517ace6122785923b565afd4ca7ae7
iton0 <iton442@gmail.com>
2023-11-03 14:33:24 -0400

    Update LSP configuration in lsp.lua
    Summary:
    This commit includes updates to the LSP configuration in the `lsp.lua` file. Notable changes include the addition of new dependencies and the configuration of options for the 'williamboman/mason.nvim' plugin. The update is aimed at enhancing the LSP setup within Neovim.

--- .config/nvim/lua/kickstart/plugins/lsp.lua
+++ .config/nvim/lua/kickstart/plugins/lsp.lua
@@ -7,9 +7,34 @@ return {
     -- Automatically install LSPs to stdpath for neovim
     {
       'williamboman/mason.nvim',
-      config = true
+      -- config = true,
+      dependencies = {
+        {
+          'williamboman/mason-lspconfig.nvim',
+          opts = {
+            automatic_installation = true,
+          },
+        },
+        {
+          'WhoIsSethDaniel/mason-tool-installer.nvim',
+          opts = {
+            auto_update = true,
+            run_on_start = true,
+            start_delay = 3000,
+            debounce_hours = 5,
+          },
+        },
+      },
+      opts = {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
+        },
+      },
     },
-    'williamboman/mason-lspconfig.nvim',
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
@@ -17,7 +42,7 @@ return {
       'j-hui/fidget.nvim',
       tag = 'legacy',
       event = 'LspAttach',
-      opts = {}
+      opts = {},
     },
 
     -- Additional lua configuration, makes nvim stuff amazing!
@@ -26,9 +51,9 @@ return {
       opts = {
         library = {
           plugins = { 'nvim-dap-ui' },
-          types = true
+          types = true,
         },
-      }
+      },
     },
   },
 }
