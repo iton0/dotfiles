bc7ea2c2666d58f7c2bac3d4d3a5cce7c5655933
iton0 <iton442@gmail.com>
2023-11-03 21:57:59 -0400

    refactor(nvim): Update which-key plugin configuration
    Made changes to the configuration of the 'folke/which-key.nvim' plugin in 'whichkey.lua'.
    
    Changes include:
    - Updated the 'event' key to 'VeryLazy' to delay plugin loading.
    - Set 'timeout' options for improved keybind display.
    - Documented and organized key chains for better understanding.
    
    This commit enhances the which-key plugin configuration in the Neovim setup.

--- .config/nvim/lua/iton/plugins/whichkey.lua
+++ .config/nvim/lua/iton/plugins/whichkey.lua
@@ -1,14 +1,14 @@
 -- Useful plugin to show you pending keybinds.
 return {
   'folke/which-key.nvim',
-  event = { 'BufReadPost', 'BufNewFile' },
+  event = 'VeryLazy',
   init = function()
     vim.o.timeout = true
     vim.o.timeout = 300
   end,
   config = function()
     -- document existing key chains
-    require('which-key').register {
+    require('which-key').register({
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
@@ -16,6 +16,6 @@ return {
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    }
+    })
   end,
 }
