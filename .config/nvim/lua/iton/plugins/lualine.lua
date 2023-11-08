de5719a3eabf49a40097531bc2521ccde8ff0e2f
iton0 <iton442@gmail.com>
2023-11-08 09:51:49 -0500

    refactor(lualine): change section separators
    Changed section seperator to no space
    
    Give my lualine a cleaner look

--- .config/nvim/lua/iton/plugins/lualine.lua
+++ .config/nvim/lua/iton/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
       icons_enabled = true,
       theme = 'auto',
       component_separators = '',
-      section_separators = ' ',
+      section_separators = '',
     },
     sections = {
       lualine_a = {
