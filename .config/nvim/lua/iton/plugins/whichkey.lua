24cfd0ac8dee8be95faf347bf91e4d51d6f91eb1
iton0 <iton442@gmail.com>
2023-12-08 12:16:23 -0500

    update: change event for whichkey.lua
    - Changed from 'ColorScheme' to 'VeryLazy'
    
    This is what is used as the example in the plugin repo

--- .config/nvim/lua/iton/plugins/whichkey.lua
+++ .config/nvim/lua/iton/plugins/whichkey.lua
@@ -1,7 +1,7 @@
 -- Useful plugin to show you pending keybinds.
 return {
   'folke/which-key.nvim',
-  event = 'ColorScheme',
+  event = 'VeryLazy',
   init = function()
     vim.o.timeout = true
     vim.o.timeout = 300
