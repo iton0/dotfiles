b27cd4b8c561b907f16e7a9766a89a0709ecbdc7
iton0 <iton442@gmail.com>
2023-12-15 23:31:58 -0500

    feat(nvim): Update Code Runner key bindings
    Revised key bindings in 'coderunner.lua' for Code Runner plugin to enhance usability and consistency. Changed the key mappings associated with running code, file, project, and closing the Code Runner window. Updated '<leader>c' key to trigger code execution, offering more intuitive and descriptive mappings ('<leader>cc', '<leader>cf', '<leader>cp', '<leader>cl') for running code, files, projects, and closing the Code Runner window respectively.

--- .config/nvim/lua/iton/plugins/coderunner.lua
+++ .config/nvim/lua/iton/plugins/coderunner.lua
@@ -1,7 +1,7 @@
 return {
   'CRAG666/code_runner.nvim',
   keys = {
-    { '<leader>x', desc = 'Code Runner' },
+    { '<leader>c' },
   },
   config = function()
     require('code_runner').setup({
@@ -32,9 +32,9 @@ return {
         },
       },
     })
-    vim.keymap.set('n', '<leader>xc', ':RunCode<CR>', { noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>xf', ':RunFile<CR>', { noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>xp', ':RunProject<CR>', { noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>xx', ':RunClose<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>cc', ':RunCode<CR>', { noremap = true, silent = false, desc = 'Run Code' })
+    vim.keymap.set('n', '<leader>cf', ':RunFile<CR>', { noremap = true, silent = false, desc = 'Run File' })
+    vim.keymap.set('n', '<leader>cp', ':RunProject<CR>', { noremap = true, silent = false, desc = 'Run Project' })
+    vim.keymap.set('n', '<leader>cl', ':RunClose<CR>', { noremap = true, silent = false, desc = 'Close Code Runner' })
   end,
 }
