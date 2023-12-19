f1e8dd1a873c68578a3c9b3544e1f2825e3409bf
iton0 <iton442@gmail.com>
2023-12-19 14:43:37 -0500

    refactor(nvim): Update Harpoon plugin to harpoon-core.nvim
    Refactor configuration in harpoon.lua to switch from 'ThePrimeagen/harpoon'
    to 'MeanderingProgrammer/harpoon-core.nvim', adjusting the key mappings
    and core functionalities for a more streamlined usage of the Harpoon-like
    plugin, focusing solely on its core features.

--- .config/nvim/lua/iton/plugins/harpoon.lua
+++ .config/nvim/lua/iton/plugins/harpoon.lua
@@ -1,5 +1,6 @@
+-- Neovim harpoon like plugin, but only the core bits
 return {
-  'ThePrimeagen/harpoon',
+  'MeanderingProgrammer/harpoon-core.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
@@ -7,33 +8,61 @@ return {
     { '<leader>h', desc = 'Harpoon' },
     {
       '<leader>hm',
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      desc = 'Mark file with harpoon',
+      "<cmd>lua require('harpoon-core.mark').add_file()<cr>",
+      desc = 'Add mark',
+    },
+    {
+      '<leader>hr',
+      "<cmd>lua require('harpoon-core.mark').rm_file()<cr>",
+      desc = 'Remove mark',
     },
     {
       '<leader>hu',
-      "<cmd>:lua require('harpoon.ui').toggle_quick_menu()<cr>",
+      "<cmd>:lua require('harpoon-core.ui').toggle_quick_menu()<cr>",
       desc = 'View marks',
     },
     {
       '<leader>1',
-      "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-      desc = 'Navigate to File 1',
+      "<cmd>lua require('harpoon-core.ui').nav_file(1)<cr>",
+      desc = 'Mark 1',
     },
     {
       '<leader>2',
-      "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-      desc = 'Navigate to File 2',
+      "<cmd>lua require('harpoon-core.ui').nav_file(2)<cr>",
+      desc = 'Mark 2',
     },
     {
       '<leader>3',
-      "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-      desc = 'Navigate to File 3',
+      "<cmd>lua require('harpoon-core.ui').nav_file(3)<cr>",
+      desc = 'Mark 3',
     },
     {
       '<leader>4',
-      "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
-      desc = 'Navigate to File 4',
+      "<cmd>lua require('harpoon-core.ui').nav_file(4)<cr>",
+      desc = 'Mark 4',
     },
   },
+  config = function()
+    require('harpoon-core').setup({
+      -- Make existing window active rather than creating a new window
+      use_existing = true,
+      -- Default action when opening a mark, defaults to current window
+      -- Example: 'vs' will open in new vertical split, 'tabnew' will open in new tab
+      default_action = nil,
+      -- Set marks specific to each git branch inside git repository
+      mark_branch = false,
+      -- Use the previous cursor position of marked files when opened
+      use_cursor = true,
+      -- Settings for popup window
+      menu = {
+        width = 60,
+        height = 10,
+      },
+      -- Highlight groups to use for various components
+      highlight_groups = {
+        window = 'HarpoonWindow',
+        border = 'HarpoonBorder',
+      },
+    })
+  end,
 }
