1ed7767753a3e4b35e7d397aeea3af3c801017ca
iton0 <iton442@gmail.com>
2023-12-26 13:34:39 -0500

    refactor: Remove unused commands in dressing.lua
    Removed unused commands related to Lazy.nvim in dressing.lua

--- .config/nvim/lua/iton/plugins/dressing.lua
+++ .config/nvim/lua/iton/plugins/dressing.lua
@@ -9,9 +9,6 @@ return {
   -- dressing.nvim plugin
   vim.keymap.set('n', '<leader>l', '', { noremap = true, silent = true, desc = 'Lazy.nvim' }),
   vim.keymap.set('n', '<leader>ls', ':Lazy sync <cr>', { noremap = true, silent = true, desc = 'Sync' }),
-  vim.keymap.set('n', '<leader>lu', ':Lazy update <cr>', { noremap = true, silent = true, desc = 'Update' }),
-  vim.keymap.set('n', '<leader>li', ':Lazy install <cr>', { noremap = true, silent = true, desc = 'Install' }),
-  vim.keymap.set('n', '<leader>lx', ':Lazy clean <cr>', { noremap = true, silent = true, desc = 'Clean' }),
   vim.keymap.set('n', '<leader>ll', ':Lazy <cr>', { noremap = true, silent = true, desc = 'Toggle Home' }),
   vim.keymap.set('n', '<leader>lp', ':Lazy profile<cr>', { noremap = true, silent = true, desc = 'Profile' }),
 }
