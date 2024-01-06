2accea889480dc4a564bff1bfefcd1495df3477d
iton0 <iton442@gmail.com>
2024-01-06 04:49:25 -0500

    refactor: Comment out 'tpope/vim-rhubarb' plugin in init.lua
    Commented out the 'tpope/vim-rhubarb' plugin in the init.lua file to disable its loading. This plugin related to Git functionality is temporarily deactivated and no longer triggered on BufReadPost and BufNewFile events.

--- .config/nvim/lua/iton/plugins/init.lua
+++ .config/nvim/lua/iton/plugins/init.lua
@@ -1,13 +1,13 @@
 -- Plugins that do not need any extra configuration
 return {
   -- Git related plugins
-  {
-    'tpope/vim-rhubarb',
-    event = { 'BufReadPost', 'BufNewFile' },
-    -- dependencies = {
-    --   'tpope/vim-fugitive',
-    -- },
-  },
+  -- {
+  --   'tpope/vim-rhubarb',
+  --   event = { 'BufReadPost', 'BufNewFile' },
+  --   dependencies = {
+  --     'tpope/vim-fugitive',
+  --   },
+  -- },
 
   -- Detect tabstop and shiftwidth automatically
   {
