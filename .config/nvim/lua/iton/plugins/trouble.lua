2e4e15842afd0f187fb541a887e1573b3ed60160
iton0 <iton442@gmail.com>
2024-01-12 09:18:08 -0500

    refactor: improve LSP setup and keymap handling
    In the keymaps.lua file, refactor autocmds related to LSP setup and keymaps. Update autocmd events to include WinEnter and FileType for comprehensive coverage. Handle excluded filetypes and file extensions more efficiently, skipping unnecessary processing. Adjust specific plugins like Harpoon, Trouble, and others. This refactoring improves the efficiency of LSP setup and keymap handling, ensuring a smoother experience when working with different filetypes and plugins.

--- .config/nvim/lua/iton/plugins/trouble.lua
+++ .config/nvim/lua/iton/plugins/trouble.lua
@@ -1,10 +1,6 @@
 return {
   'folke/trouble.nvim',
-  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     auto_close = true,
     icons = false,
   },
@@ -12,6 +8,6 @@ return {
     'n',
     '<leader>q',
     "<cmd>lua require('trouble').toggle()<cr>",
-    { desc = 'Diagnostics list' }
+    { desc = 'Trouble' }
   ),
 }
