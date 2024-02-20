cbcaf8a764dbb5a6e84ebac5f25771ceb59b76a3
iton0 <iton442@gmail.com>
2024-02-20 12:30:40 -0500

    feat(git): Add configuration for git worktrees in Neovim
    - Setup git worktree configuration to disable auto-update
    - Define functions to handle git worktree changes
    - Create Tmux windows based on worktree branches
    - Switch to existing Tmux window if it exists
    
    This configuration enhances Neovim's integration with git worktrees, allowing seamless switching and creation of Tmux windows corresponding to worktree branches.

--- .config/nvim/lua/iton/plugins/git.lua
+++ .config/nvim/lua/iton/plugins/git.lua
@@ -56,7 +56,89 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope.nvim',
     },
-    opts = {},
+    config = function()
+      local Worktree = require('git-worktree')
+      Worktree.setup({ update_on_change = false })
+      -- NOTE:
+      -- op = Operations.Switch, Operations.Create, Operations.Delete
+      -- metadata = table of useful values (structure dependent on op)
+      --      Switch
+      --          path = path you switched to
+      --          prev_path = previous worktree path
+      --      Create
+      --          path = path where worktree created
+      --          branch = branch name
+      --          upstream = upstream remote name
+      --      Delete
+      --          path = path where worktree deleted
+
+      local getFirstWorktreePath = function()
+        -- Run git worktree list to get a list of worktrees
+        local handle = io.popen('git worktree list')
+
+        if not handle then
+          print('Error opening git worktree list.')
+          return nil
+        end
+
+        local result = handle:read('*a')
+        local success, reason = handle:close()
+
+        if not success then
+          print('Error reading git worktree list:', reason)
+          return nil
+        end
+
+        -- Extract the first worktree path
+        local firstPath = result:match('(%S+)')
+
+        return firstPath
+      end
+
+      Worktree.on_tree_change(function(op, metadata)
+        if op == Worktree.Operations.Create then
+          local tmuxWindowName = metadata.branch
+          local firstPath = getFirstWorktreePath()
+          if firstPath then
+            -- Replace the last component with metadata.branch
+            local tmuxStartingDirectory =
+              firstPath:gsub('/[^/]+$', '/' .. metadata.branch)
+
+            -- Create and switch to a new Tmux window
+            local tmuxCommand = 'tmux new-window -n '
+              .. tmuxWindowName
+              .. ' -c '
+              .. tmuxStartingDirectory
+            os.execute(tmuxCommand)
+          else
+            print('No worktrees found.')
+          end
+        end
+      end)
+      Worktree.on_tree_change(function(op, metadata)
+        if op == Worktree.Operations.Switch then
+          local tmuxStartingDirectory = metadata.path
+          local tmuxWindowName = vim.fn.fnamemodify(metadata.path, ':t')
+
+          -- Check if a Tmux window with the same name already exists
+          local existingWindow = os.execute(
+            'tmux list-windows -F "#W" | grep -wq ' .. tmuxWindowName
+          )
+
+          if existingWindow ~= 0 then
+            -- Tmux window with the same name does not exist, create a new one
+            local tmuxCommand = 'tmux new-window -n '
+              .. tmuxWindowName
+              .. ' -c '
+              .. tmuxStartingDirectory
+            os.execute(tmuxCommand)
+          else
+            -- Tmux window with the same name already exists, switch to it
+            os.execute('tmux select-window -t ' .. tmuxWindowName)
+          end
+        end
+      end)
+    end,
     vim.keymap.set(
       'n',
       '<leader>st',
