c9130af97982ab92f109143c6b0d20d367fbcd74
iton0 <iton442@gmail.com>
2024-02-20 14:39:54 -0500

    refactor: Update colorscheme and lualine configurations
    Adjust the colorscheme and lualine configurations in the Neovim setup. This commit introduces a lighter background for the CursorLineNr, removes unnecessary color adjustments, and enhances the Visual highlight. The lualine theme is now set to 'codedark' for a cleaner appearance.
    
    Changes:
    - Adjust CursorLineNr to use bold formatting and remove specific color settings
    - Enhance Visual highlight with a lighter background
    - Set lualine theme to 'codedark' for a cleaner appearance
    
    These changes improve the overall aesthetics and readability of Neovim.

--- .config/nvim/lua/iton/plugins/lualine.lua
+++ .config/nvim/lua/iton/plugins/lualine.lua
@@ -6,54 +6,7 @@ return {
   config = function()
     require('lualine').setup({
       options = {
-        theme = function()
-          local colors = {
-            blue = '#268bd2',
-            green = '#859900',
-            purple = '#6c71c4',
-            cyan = '#2aa198',
-            red1 = '#dc322f',
-            red2 = '#cb4b16',
-            yellow = '#b58900',
-            fg = '#93a1a1',
-            bg = '#002b36',
-            gray1 = '#657b83',
-          }
-          return {
-            normal = {
-              a = { fg = colors.bg, bg = colors.blue, gui = 'bold' },
-              b = { fg = '#d8b56d', bg = nil },
-              c = { fg = colors.fg, bg = nil },
-              x = { fg = colors.fg, bg = nil },
-              y = { fg = colors.fg, bg = nil },
-            },
-            command = {
-              a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
-              y = { fg = colors.fg, bg = colors.bg },
-            },
-            insert = {
-              a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
-              y = { fg = colors.fg, bg = colors.bg },
-            },
-            visual = {
-              a = { fg = colors.bg, bg = colors.purple, gui = 'bold' },
-              y = { fg = colors.fg, bg = colors.bg },
-            },
-            terminal = {
-              a = { fg = colors.bg, bg = colors.cyan, gui = 'bold' },
-              y = { fg = colors.fg, bg = colors.bg },
-            },
-            replace = {
-              a = { fg = colors.bg, bg = colors.red1, gui = 'bold' },
-              y = { fg = colors.fg, bg = colors.bg },
-            },
-            inactive = {
-              a = { fg = colors.gray1, bg = colors.bg },
-              b = { fg = colors.gray1, bg = colors.bg },
-              c = { fg = colors.gray1, bg = colors.bg },
-            },
-          }
-        end,
+        theme = 'codedark',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
       },
