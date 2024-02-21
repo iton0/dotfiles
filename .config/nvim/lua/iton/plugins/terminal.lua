e7bd785c3fc3ad99756e28039bcfd70cf9d3567d
iton0 <iton442@gmail.com>
2024-02-21 12:34:43 -0500

    refactor(terminal): remove size and direction options
    Removed the `size` and `direction` options from the configuration for
    `akinsho/toggleterm.nvim` in order to streamline the terminal settings
    and enhance simplicity.

--- .config/nvim/lua/iton/plugins/terminal.lua
+++ .config/nvim/lua/iton/plugins/terminal.lua
@@ -3,8 +3,6 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    size = 50,
-    direction = 'vertical',
     auto_scroll = false,
     autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
   },
