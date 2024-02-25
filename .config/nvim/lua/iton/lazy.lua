6fb95e06fd402baaf505b1f3bee181871ab0dc7b
iton0 <iton442@gmail.com>
2024-02-25 17:49:32 -0500

    refactor: move initialization of leader key
    Changes:
    - Set <space> as the leader key in lua/iton/core/options.lua
    - Removed leader key settings in lua/iton/lazy.lua

--- .config/nvim/lua/iton/lazy.lua
+++ .config/nvim/lua/iton/lazy.lua
@@ -1,9 +1,3 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
