fcda782389a0e271520bae035b1fd7dfad6f3128
iton0 <iton442@gmail.com>
2024-02-27 16:10:52 -0500

    refactor(nvim): remove redundant initialization in conform plugin
    Removed keymapping for formatting. Formatting is now done only on BufReadPre.

--- .config/nvim/lua/iton/plugins/conform.lua
+++ .config/nvim/lua/iton/plugins/conform.lua
@@ -2,10 +2,6 @@ return {
   'stevearc/conform.nvim',
   event = 'BufWritePre',
   cmd = 'ConformInfo',
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
   opts = {
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
     notify_on_error = false,
@@ -21,10 +17,4 @@ return {
       ['_'] = { 'trim_whitespace' },
     },
   },
-  vim.keymap.set(
-    { 'n', 'v' },
-    '<leader>t',
-    '<cmd>lua require("conform").format({ lsp_fallback = true, timeout_ms = 500 })<cr>',
-    { desc = 'Format' }
-  ),
 }
