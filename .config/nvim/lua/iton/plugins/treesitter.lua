a577156b26c70798f2a58534e4ad37a9772b1414
iton0 <iton442@gmail.com>
2024-03-24 21:31:55 -0400

    feat(plugins): Add treesitter configuration with specified languages and highlighting options

--- .config/nvim/lua/iton/plugins/treesitter.lua
+++ .config/nvim/lua/iton/plugins/treesitter.lua
@@ -3,13 +3,28 @@ return {
   'nvim-treesitter/nvim-treesitter',
   event = { 'BufReadPost', 'BufNewFile' },
   build = ':TSUpdate',
-  config = function()
+  opts = {
+    ensure_installed = {
+      'bash',
+      'c',
+      'html',
+      'lua',
+      'markdown',
+      'vim',
+      'vimdoc',
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+  },
+  config = function(_, opts)
     ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'lua' },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+    require('nvim-treesitter.configs').setup(opts)
   end,
 }
