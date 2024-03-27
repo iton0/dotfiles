c6baaa8a5a2563ad68294f48e36628c1d89dba1b
iton0 <iton442@gmail.com>
2024-03-26 21:41:06 -0400

    fix: Update treesitter plugin configuration
    This commit updates the configuration for the treesitter plugin in the treesitter.lua file. It sets sync_install to true to ensure that installations are synchronized, and adds 'tmux' and 'gitcommit' to the list of disabled languages for highlighting to prevent weird indenting issues.

--- .config/nvim/lua/iton/plugins/treesitter.lua
+++ .config/nvim/lua/iton/plugins/treesitter.lua
@@ -13,6 +13,7 @@ return {
       'vim',
       'vimdoc',
     },
+    sync_install = true,
     auto_install = true,
     highlight = {
       enable = true,
@@ -20,6 +21,7 @@ return {
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
+      disable = { 'tmux', 'gitcommit' },
     },
     indent = { enable = true, disable = { 'ruby' } },
   },
