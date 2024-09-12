b9c2740db2dd384d07f9aa58e5e586132e17a113
iton0 <iton442@gmail.com>
2024-09-12 15:34:58 -0400

    style(nvim): colorscheme toggler
    added a colorscheme toggler for solarized to switch between light and
    dark
    
    Makes neovim readable when in bright environment and easily switchable.
    Default background option in neovim still is 'dark'

--- .config/nvim/lua/iton/plugins/colorscheme.lua
+++ .config/nvim/lua/iton/plugins/colorscheme.lua
@@ -4,9 +4,25 @@ return {
   init = function()
     require('solarized').setup({
       transparent = {
-        enabled = true,
         pmenu = false,
       },
+      on_highlights = function()
+        local groups = {
+          SignColumn = { bg = 'NONE' },
+          LineNr = { bg = 'NONE' },
+          WinSeparator = { bg = 'NONE' },
+          DiagnosticSignError = { bg = 'NONE' },
+          DiagnosticSignHint = { bg = 'NONE' },
+          DiagnosticSignWarn = { bg = 'NONE' },
+          DiagnosticSignInfo = { bg = 'NONE' },
+          DiagnosticSignOk = { bg = 'NONE' },
+          GitSignsAdd = { bg = 'NONE' },
+          GitSignsChange = { bg = 'NONE' },
+          GitSignsDelete = { bg = 'NONE' },
+        }
+
+        return groups
+      end,
       styles = {
         types = { italic = false, bold = false },
         functions = { italic = false, bold = false },
@@ -18,6 +34,20 @@ return {
         constants = { italic = false, bold = false },
       },
     })
+    -- Set the initial colorscheme
     vim.cmd.colorscheme('solarized')
+
+    -- Function to toggle background
+    local function toggle_background()
+      if vim.o.background == 'dark' then
+        vim.o.background = 'light'
+      else
+        vim.o.background = 'dark'
+      end
+      vim.cmd.colorscheme('solarized') -- Reload colorscheme to apply the new background
+    end
+
+    -- Set keymap to toggle background
+    vim.keymap.set('n', '<M-t>', toggle_background)
   end,
 }
